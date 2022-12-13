e6e51d7a5cfa0fa5dc1910abc19360620b09da5d
Michael <rgfb35@durham.ac.uk>
2022-12-13 09:56:01 +0000

    Add project files.

---
+++ DataMasker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataMasker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Expected Arguments: [JSON Path] [Rules Path]");
+                Console.ReadKey();
+            }
+            else
+            {
+                string jsonPath = args[0];
+                string rulesPath = args[1];
+            }
+        }
+    }
+}
