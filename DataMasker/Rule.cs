1c47c271a89a0edb5367f00ec1d355ff6d93d6b3
Michael <rgfb35@durham.ac.uk>
2022-12-30 11:24:59 +0000

    Add Rule class to store hashsets of rule patterns

---
+++ DataMasker/Rule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DataMasker
+{
+    // Stores Regex patterns read in from a rules JSON file
+    public class Rule
+    {
+        public HashSet<Regex> KeyPatterns { get; set; }
+
+        public HashSet<Regex> ValuePatterns { get; set; }
+
+        public Rule()
+        {
+            KeyPatterns = new HashSet<Regex>();
+            ValuePatterns = new HashSet<Regex>();
+        }
+
+    }
+}
