0b3e46c67a680eb9ab1fe0f39f818ea7c1f5313f
Michael <rgfb35@durham.ac.uk>
2022-12-31 10:51:20 +0000

    Add Utils functions to data to write new JSON file

--- DataMasker/Program.cs
+++ DataMasker/Program.cs
@@ -1,20 +1,46 @@
-﻿using System;
-
+﻿
+using Newtonsoft.Json.Linq;
 namespace DataMasker
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             if (args.Length != 2)
             {
                 Console.WriteLine("Expected Arguments: [JSON Path] [Rules Path]");
-                Console.ReadKey();
+                return;
             }
             else
             {
-                string jsonPath = args[0];
+                // Get file paths
+                string dataPath = args[0];
                 string rulesPath = args[1];
+
+                // Exit if either file path is not valid
+                if (!File.Exists(dataPath) || !File.Exists(rulesPath))
+                {
+                    Console.WriteLine("One or both of the file paths could not be found.");
+                    return;
+                }
+
+
+                Rule rule = MaskingUtils.MakeRule(rulesPath);
+
+                JArray dataArray = MaskingUtils.LoadData(dataPath);
+
+                MaskingUtils.MaskData(dataArray, rule);
+
+                // Build destination path for output file
+                string dirName = Path.GetDirectoryName(dataPath);
+                string dataFileName = Path.GetFileName(dataPath);
+
+                string outputFilePath  = Path.Combine(dirName, "masked_" + dataFileName);
+
+                // Write new masked data to file
+                MaskingUtils.WriteMaskedDataToFile(dataArray, outputFilePath);
+
+
             }
         }
     }
